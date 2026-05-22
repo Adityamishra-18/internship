23a65cc6f8772fdac28b3b6b657a8c3e0b612843
Adityamishra-18 <madityamishra05@gmail.com>
2026-05-22 10:57:34 +0530

    Update swapusing_third.java

--- first_java/swapusing_third.java
+++ first_java/swapusing_third.java
@@ -1,1 +1,17 @@
+package com.internship;
 
+public class swapusing_third {
+	public static void main(String[] args) {
+		 int a = 3;
+	     int b = 5;
+
+	     System.out.println("Before Swapping the value of a = "+a+" and b = "+ b);
+
+	     int temp = a;
+	     a = b;
+	     b = temp;
+
+	     System.out.println("After Swapping the value of a = "+a+" and b = "+ b);
+	}
+
+}
