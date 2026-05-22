3d18c664b4421e7286efb56593dfd1f23aefdc13
Adityamishra-18 <madityamishra05@gmail.com>
2026-05-22 10:51:37 +0530

    Update primitiveDatatypes.java

--- first_java/primitiveDatatypes.java
+++ first_java/primitiveDatatypes.java
@@ -1,1 +1,25 @@
+package com.internship;
 
+public class primitiveDatatypes {
+	public static void main(String[] args) {
+		byte b = 10;
+        short s = 30000;
+        int i = 300;
+        long l = 250320100002L;
+        float f = 5.75f;
+        double d = 19.99999;
+        char ch = 'A';
+        boolean dec= true;
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + ch);
+        System.out.println("Boolean: " + dec);
+
+	}
+
+}
